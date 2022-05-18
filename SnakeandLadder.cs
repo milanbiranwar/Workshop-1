0a349420a8998d862b5b41cb77b874fad3fc3ef3
milanbiranwar <102604173+milanbiranwar@users.noreply.github.com>
2022-05-18 11:11:38 +0530

    Add files via upload

---
+++ SnakeandLadder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkShop
+{
+    internal class SnakeandLadder
+    {
+        public static void SinglePlayer()
+        {
+            int position = 0;
+            Console.WriteLine("You are at Starting Position: " + position);
+        }
+    }
+}
